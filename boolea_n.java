63779767e65a93b08e8b75a2b0f4d87f24205559
HimanshuKharkwal126 <126himanshu@gmail.com>
2024-01-23 11:43:03 +0530

    Adding Java Programs

---
+++ boolea_n.java
@@ -0,0 +1,25 @@
+class boolea_n {
+    public static void main(String[] args) {
+        // int a=12;
+        // int b=5;
+        // int c=a+b;
+        // int d=a-b;
+        // int e=a*b;
+        // float f=(a/b);
+        // System.out.println("sum of a and b is:"+c);
+        // System.out.println("diff of a and b is:"+d);
+        // System.out.println("multiplication of a and b is:"+e);
+        // System.out.println("div of a and b is:"+f);
+        // System.out.println("remainder of a and b is:"+(a%b));
+
+
+        int a=15;
+        int b=10;
+        boolean c=a<b;
+        boolean d=a==b;
+        System.out.println(c);
+        System.out.println(d);
+        a*=3%5-3;
+        System.out.println(a);
+        }
+}
