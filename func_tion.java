6b1c561c9ea13e0afa2f6d1e8e846e73567ddaa1
HimanshuKharkwal126 <149239447+HimanshuKharkwal126@users.noreply.github.com>
2024-02-02 21:12:10 +0530

    Add files via upload

---
+++ func_tion.java
@@ -0,0 +1,64 @@
+ /*
+                                            // print name:
+import java.util.*;
+class func_tion{
+    public static void printMyName(String name){
+        System.out.println(name);
+        return;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the name:");
+        String name=sc.nextLine();
+        printMyName(name);
+        System.out.print("your name is:"+name);
+    }
+} 
+
+*/
+ 
+                                // sum of two numbers:
+/* 
+import java.util.Scanner;
+class func_tion{
+    public static int calculate_sum(int a,int b){
+        int sum=a+b;
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the first number:");
+        int a=sc.nextInt();
+        System.out.print("enter the second number:");
+        int b=sc.nextInt();
+        System.out.println("sum of two numbers is:"+calculate_sum(a,b));
+    }
+}
+
+*/
+
+
+
+                                // product of two numbers:
+
+/* 
+
+import java.util.Scanner;
+class func_tion{
+    public static int calculate_mul(int a,int b){
+        return a*b;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the first number:");
+        int a=sc.nextInt();
+        System.out.print("enter the second number:");
+        int b=sc.nextInt();
+        System.out.println("product of two numbers is:"+calculate_mul(a,b));
+    }
+}
+
+*/
+
+
+
