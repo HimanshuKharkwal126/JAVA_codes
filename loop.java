487a7231516ee0ee99453fd03d4c808b09cc372a
HimanshuKharkwal126 <149239447+HimanshuKharkwal126@users.noreply.github.com>
2024-02-08 20:51:25 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,43 @@
+                           // while loop:
+/* 
+class loop{
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=10){
+            System.out.println("hey:");
+            i++;
+        }
+    }
+}
+
+*/
+
+ 
+                          //  do while loop:
+/*
+class loop{
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println("hello world:");
+            i++;
+        }
+        while(i<=5);
+    }
+}
+
+*/
+
+                        // for loop:  reverse of 1 to 10 numbers:
+
+/* 
+
+class loop{
+    public static void main(String[] args) {
+        for(int i=10;i>=1;i--){
+            System.out.println(i);
+        }
+    }
+}
+
+*/
