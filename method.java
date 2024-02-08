d136e46741b414a4828a3f8d0c5e5f25d918d60f
HimanshuKharkwal126 <149239447+HimanshuKharkwal126@users.noreply.github.com>
2024-02-08 20:51:56 +0530

    Add files via upload

---
+++ method.java
@@ -0,0 +1,31 @@
+/* 
+
+import java.util.*;
+class method {
+    String brand;
+    String processor;
+    double price;
+    int ram;
+    int rom;
+    public static void main(String[] args) {
+        method m1=new method();
+        method m2=new method();
+        m1.brand="redmi note 8";
+        m1.processor="snapdragon 695";
+        m1.price=17000.52;
+        m1.ram=6;
+        m1.rom=128;
+        System.out.println(m1.processor);
+        m2.brand="apple";
+        m2.processor="octa_core";
+        m2.price=85000.52;
+        m2.ram=8;
+        m2.rom=256;
+        System.out.println(m1.processor);
+        System.out.println(m2.ram);
+       }
+   } 
+*/
+
+
+
