ced7f416e61469b0526f411ae1bbcac935229d43
HimanshuKharkwal126 <149239447+HimanshuKharkwal126@users.noreply.github.com>
2024-02-15 21:18:52 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,10 @@
+                     // table of given no:
+
+class table {
+   public static void main(String[] args) {
+       int a=12;
+       for(int i=1;i<=10;i++){
+           System.out.println(a+"*"+i+"="+a*i);
+       }
+   }
+}
